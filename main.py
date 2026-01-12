6789698c9935770ef53cbb8abdee08d9f54e574f
Khairul Ikhwan <khairul0355@hotmail.com>
2026-01-13 02:09:44 +0800

    seperated task and edit for compatibility

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import cv2
-import numpy as np
 
-img = cv2.imread('test.jpeg')
+import auto_transform
+import enhance
 
-# Pixel values in the original image
-input_points = np.float32([[83, 18], [342, 53], [14, 389], [295, 436]])
+img_path = 'test.jpeg'
 
-# Output image size
-width = 400
-height = int(width * 1.414)  # for A4
+img = cv2.imread(img_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Desired points values in the output image
-converted_points = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+img_enhance = enhance.enhance(img)
 
-# Perspective transformation
-matrix = cv2.getPerspectiveTransform(input_points, converted_points)
-img_output = cv2.warpPerspective(img, matrix, (width, height))
+result = auto_transform.warping(img_enhance)
 
-cv2.imshow('Original', img)
-cv2.imshow('Warped perspective', img_output)
-
-# cv2.imwrite('output/document.jpg', img_output)
+cv2.imshow("output", result)
 
 cv2.waitKey(0)
